dcd1e45f952d0d07923e1d18522973a301390f14
Eriri-Spencer20 <radityaappj23@gmail.com>
2021-12-21 15:21:34 +0700

    include home and footer

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php 
+<?php
 @ob_start();
 session_start();
 
-if(!empty($_SESSION['admin'])){
+if (!empty($_SESSION['admin'])) {
     require 'config.php';
+
     // admin
     include 'admin/template/header.php';
     include 'admin/template/sidebar.php';
+    if (!empty($_GET['page'])) {
+        include 'admin/module/'.$_GET['page'].'/index.php';
+    } else {
+        include 'admin/template/home.php';
+    }
+    include 'admin/template/footer.php';
 }
+?>
