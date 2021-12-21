957087064c2a8da6531cc0f8434364bc287a8853
Eriri-Spencer20 <radityaappj23@gmail.com>
2021-12-22 00:43:04 +0700

    membuat function view (setengah jalan)

--- function/view/view.php
+++ function/view/view.php
@@ -57,4 +57,57 @@ class view
         $hasil = $row -> fetchAll();
         return $hasil;
     }
+    function barang_edit($id){
+        $sql = "select barang.*, kategori.id_kategori, kategori.nama_kategori from barang inner join kategori on barang.id_kategori = kategori. id_kategori where id_barang=?";
+        $row = $this-> db -> prepare($sql);
+        $row -> execute(array($id));
+        $hasil = $row -> fetch();
+        return $hasil;
+    }
+    function barang_cari($cari){
+        $sql = "select barang.*, kategori.id_kategori, kategori.nama_kategori from barang inner join kategori on barang.id_kategori = kategori. id_kategori where id_barang like '%$cari%'
+        or nama_barang like '%$cari%' or merk like '%$cari%'";
+        $row = $this-> db -> prepare($sql);
+        $row -> execute();
+        $hasil = $row -> fetchAll();
+        return $hasil;
+    }
+    // function barang_id(){
+
+    // }
+    function kategori_edit($id){
+        $sql = "select*from kategori where id_kategori=?";
+        $row = $this -> db -> prepare($sql);
+        $row -> execute(array($id));
+        $hasil = $row -> fetch();
+        return $hasil;
+    }
+    function kategori_row(){
+        $sql = "select*from kategori ";
+        $row = $this -> db -> prepare($sql);
+        $row -> execute();
+        $hasil = $row -> rowCount();
+        return $hasil;
+    }
+    function barang_stok_row(){
+        $sql = "SELECT SUM(stok) as jml FROM barang";
+        $row = $this -> db -> prepare($sql);
+        $row -> execute();
+        $hasil = $row -> fetch();
+        return $hasil;
+    }
+    function barang_beli_row(){
+        $sql = "SELECT SUM(hargaz_beli) as bwli FROM barang";
+        $row = $this -> db -> prepare($sql);
+        $row -> execute();
+        $hasil = $row -> fetch();
+        return $hasil;
+    }
+    function jual_row(){
+        $sql = "SELECT SUM(jumlah) as stok FROM nota";
+        $row = $this -> db -> prepare($sql);
+        $row -> execute();
+        $hasil = $row -> fetch();
+        return $hasil;
+    }
 }
